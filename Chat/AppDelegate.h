75397fc3404ad6ee077a6ec02fe5db8003d96528
Alex Lester <alester@groupon.com>
2015-11-05 14:48:12 -0800

    Inital commit

---
+++ Chat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chat
+//
+//  Created by Alex Lester on 11/5/15.
+//  Copyright © 2015 Alex Lester. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chat
+//
+//  Created by Alex Lester on 11/5/15.
+//  Copyright © 2015 Alex Lester. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
